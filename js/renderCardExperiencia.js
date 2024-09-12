8c75004db83b1a1a925604ba1e6ca5491f2f1ba6
nathaliafreboucas <contato.nathaliareboucas@outlook.com>
2024-09-12 18:41:19 -0300

    Melhorias na renderização dos cards de projetos

---
+++ js/renderCardExperiencia.js
@@ -0,0 +1,34 @@
+
+import {dataCardExperiencias} from './data.js'
+export const renderCardExperiencia = (organizaCard, classItemExp, banner, titulo, urlRep, urlPag, logosTecDev) =>{
+    const experienciaPlace = document.querySelector('[data-experiencia]')
+    const logosHTML = logosTecDev.map(logo => `<img src="${logo}" alt="Logo de tecnologia"/>`).join('');
+    const templateCardExperiencia = `
+    <div class="section-item-experiencia ${classItemExp}">
+        <div class="div_imagem">
+            <img src="${banner}" alt="">
+        </div>
+        <div class="div_botoes" id="${organizaCard}">
+            <h3>${titulo}</h3>
+            <p>Desafio proposto pelo programa Oracle Next Education.</p>
+            <div class="logos-projetos">
+               ${logosHTML}               
+            </div>                        
+            <div>
+                <a href="${urlRep}" target="_blank"><button class="experiencia_botoes">Repositório</button></a>
+                <a href="${urlPag}" target="_blank"><button class="experiencia_botoes">Página</button></a>
+            </div>
+        </div>
+    </div>
+    `
+    experienciaPlace.innerHTML += templateCardExperiencia
+}
+
+dataCardExperiencias.map((item,index)=>{
+    let organizaCard = ""
+    if((index+1)%2==0){
+        organizaCard = "excecao-experiencia"
+    }
+
+    renderCardExperiencia(organizaCard, item.class, item.banner, item.titulo, item.urlRep, item.urlPag, item.logosTecDev)
+})
